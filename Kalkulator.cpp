979fb6d30945b12b3d7416a5df28384e48a9db94
ilhammhrrm <90167055+ilhammhrrm@users.noreply.github.com>
2021-09-30 09:43:31 +0700

    Update Kalkulator.cpp

--- Kalkulator.cpp
+++ Kalkulator.cpp
@@ -1,44 +1,50 @@
-#include<iostream>
+#include <conio.h>
+#include <iostream>
+#include <string>
+
 using namespace std;
 
 int main(){
-				int konversi, A, B;
-				float hasil;
 
-				cout<<"\tPROGRAM KALKULATOR";
-				cout<<"\nPilih Bnetuk Konversi";
-				cout<<"\n-Penjumlahan = 1\n-Pengurangan = 2\n-Perkalian = 3\n-Pembagian = 4\n-Modulo = 5";
-				cout<<"\nKonversi yang anda inginkan (DALAM BENTUK ANGKA) = ";
-				cin>> konversi;
+	int bil1,bil2, pil;
+	float hasil;
+	string operasi;
 
-				cout<<"\nMasukkan Nilai A = ";
-				cin>>A;
-				cout<<"\nMasukkan Nilai B = ";
-				cin>>B;
+	cout<<"PILIH OPERATOR ARITMATIKA"<<endl;
+	cout<<"1. Penjumlahan"<<endl;
+	cout<<"2. Pengurangan"<<endl;
+	cout<<"3. Perkalian"<<endl;
+	cout<<"4. Pembagian"<<endl;
+	cout<<"5. Modulus"<<endl;
+	cout<<endl;
 
-				switch(konversi){
-					case 1:
-							hasil = A+B;
-							break;
-					case 2:
-							hasil = A-B;
-							break;
-					case 3:
-							hasil = A*B;
-							break;
-					case 4:
-							hasil =float(A)/B;
-							hasil =A/float(B);
-							break;
-					case 5:
-							hasil = A%B;
-							break;
-					default:
-					cout<<"\n";
-					cout<<"\n\tPILIHAN KONVERSI TERSEDIA";
-				}
+	cout<<"Masukan Pilihan : ";
+	cin>>pil;
+	cout<<"Masukan Bilangan pertama : ";
+	cin>>bil1;
+	cout<<"Masukan Bilangan kedua : ";
+	cin>>bil2;
 
+	switch(pil){
+	        case 1 : hasil=bil1+bil2;
+			operasi='+';
+			break;
+		case 2 : hasil=bil1-bil2;
+			operasi='-';
+			break;
+		case 3 : hasil=bil1*bil2;
+			operasi='*';
+			break;
+		case 4 : hasil=bil1/bil2;
+			operasi='/';
+			break;
+		case 5 : hasil=bil1%bil2;
+			 operasi='%';
+			break;
+		default :
+			cout<<"Salah Masukan Operator"<<endl;
+	}
+	cout<<"    "<<bil1<<operasi<<bil2<<"="<<hasil<<endl;
 
-cout<<"\nHASIL KONVERSI	= "<<hasil;
-return 0;
+	getch();
 }
